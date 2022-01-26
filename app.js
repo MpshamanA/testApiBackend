475365bd574f5ed6db00a42ca6d5b761a886f16d
shoIwasaki <>
2022-01-27 00:20:46 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+
+const mysql = require("mysql");
+const con = mysql.createConnection({
+  host: HOSTNAME,
+  user: USER,
+  password: PASSWORD,
+  database: DATABASENAME,
+});
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+//SQLサーバーへの接続
+con.connect((err) => {
+  if (err) throw err;
+  console.log("Connected");
+});
+
+//レスポンスとしてSQLの実行結果を画面に表示
+app.get("/api", (req, res) => {
+  const sql = "select * from users";
+  con.query(sql, (err, result, fields) => {
+    if (err) throw err;
+    res.json(result);
+    // res.json({ message: "Hello World!" });
+  });
+});
+//Serverの起動
+app.listen(port, () => {
+  console.log(`OK localhost:${port}`);
+});
