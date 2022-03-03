2ca101ada84f8d52987c8b00eb303b37719c5ebd
shoIwasaki <m.pshaman.a@gmail.com>
2022-03-04 00:14:10 +0900

    postロジック追加

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ const port = 3001;
 app.use(express.json());
 
 const mysql = require("mysql");
-const con = mysql.createConnection({
-  host: HOSTNAME,
-  user: USER,
-  password: PASSWORD,
-  database: DATABASENAME,
-});
+const con = mysql.createConnection({});
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //SQLサーバーへの接続
 con.connect((err) => {
@@ -18,12 +17,35 @@ con.connect((err) => {
   console.log("Connected");
 });
 
+app.post("/api", (req, res) => {
+  const request = req.body;
+  console.log(request);
+  const name = request.name;
+  const position = request.position;
+  //パターン1
+  // const sqlInsert1 = `insert into users (name,position) VALUES ("${name}","${position}")`;
+  // con.query(sqlInsert1, (err, result, fields) => {
+  //   if (err) throw err;
+  //   console.log(result);
+  // });
+
+  //パターン2
+  //?の場合はquetyの引数で値を渡す
+  // const sqlInsert2 = `insert into users (name,position) VALUES (?,?)`;
+  const sqlSelect = `select * from users where name = ?`;
+  con.query(sqlSelect, [name], (err, result, fields) => {
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
 //レスポンスとしてSQLの実行結果を画面に表示
 app.get("/api", (req, res) => {
-  const sql = "select * from users";
+  const sql = `select * from users`;
   con.query(sql, (err, result, fields) => {
     if (err) throw err;
     res.json(result);
+
     // res.json({ message: "Hello World!" });
   });
 });
